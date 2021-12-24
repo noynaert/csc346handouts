338ebec0006d5ec5e1bffd51f6206f1728e7a8de
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-24 14:49:49 -0600

    start 03 Reading from a file

--- 00_Getting_Started/sourceCode/helloWorld/src/App.java
+++ 00_Getting_Started/sourceCode/helloWorld/src/App.java
@@ -1,6 +1,11 @@
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+    
+        System.out.println("Starting...");
+    
+        String[] list = new String[10];
+        System.out.println(list[10]);
+
         System.out.println("Done!");
     }
 }
