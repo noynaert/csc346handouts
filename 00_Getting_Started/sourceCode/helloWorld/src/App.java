8cd31f1ffdd9402ec40e397a4c3eaa8d074ad22c
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-24 15:18:11 -0600

    add url to notes

--- 00_Getting_Started/sourceCode/helloWorld/src/App.java
+++ 00_Getting_Started/sourceCode/helloWorld/src/App.java
@@ -2,9 +2,6 @@ public class App {
     public static void main(String[] args){
     
         System.out.println("Starting...");
-    
-        String[] list = new String[10];
-        System.out.println(list[10]);
 
         System.out.println("Done!");
     }
