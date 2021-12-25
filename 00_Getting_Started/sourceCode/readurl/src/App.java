fac7db904df0149893726daecbac9e4093145117
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-25 07:52:11 -0600

    start code for reading from a URL

---
+++ 00_Getting_Started/sourceCode/readurl/src/App.java
@@ -0,0 +1,15 @@
+
+/** 
+ * Reading from a URL
+ * 
+ * @author J. Evan Noynaert
+ * @since December 2021
+ * 
+ * Reads from a file and handles exceptions
+ * 
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nDone!");
+    }
+}
