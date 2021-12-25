837594b1ee621291c40866c196865815ff4bebd2
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-12-25 12:50:29 -0600

    Add readurl source code

--- 00_Getting_Started/sourceCode/readurl/src/App.java
+++ 00_Getting_Started/sourceCode/readurl/src/App.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
