c543ac74a2daa5b9d6625b926ab054a1e46de930
J. Evan Noynaert <noynaert@missouriwestern.edu>
2022-01-21 15:33:32 -0600

    add javadoc to Logger

--- 01_DataExchange/sourceCode/01_Utility/src/CSC346Utility/Logger.java
+++ 01_DataExchange/sourceCode/01_Utility/src/CSC346Utility/Logger.java
@@ -4,10 +4,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
+/**
+ * Logger class provides very basic and simple logging
+ * 
+ * @author J. Evan Noynaert
+ * @since January 2022
+ * 
+ */
 public class Logger {
     private static String name;
     private static PrintStream logFile = null;
     private static long startTime = java.lang.System.nanoTime();
+
+    /**
+     * The contructor takes the root name of the log file.  The extension .log will be appended to the root.
+     * 
+     * @param name: String.  This will be the root of the file name.
+     */
 
     public Logger(String name) {
         Logger.name = name;
@@ -27,28 +40,52 @@ public class Logger {
         }
     }
 
+    /**
+     * MakeMessage appends the time to the beginning of the message
+     * 
+     * @param message: String with content of the message
+     * @return A formatted string containing the timestamp and message
+     */
     private static String makeMessage(String message) {
         String s = String.format("%1.4f -->%s<--", getSeconds(), message);
         return s;
     }
 
+    
+    /** 
+     * @return double
+     */
     public static double getSeconds() {
         long currentTime = System.nanoTime();
         double seconds = (double) (currentTime - startTime)/1000000000.0;
         double sec = (double) seconds / 1000000.;        return seconds;
     }
 
+    /**
+     * This is the workhorse of the logger.  It may be used at any time
+     * @param message: String Contains the message to be logged.
+     */
     public static void info(String message) {
         logFile.println(makeMessage(message));
         logFile.flush();
     }
 
+
+    /**
+     * This method is intended for use in a catch{} block.
+     * 
+     * @param e:Exception The parameter should be an Exception or a subclass of
+     *                    Exception.
+     */
     public static void exception(Exception e) {
         String message = "EXCEPTION: " + e.getMessage();
         logFile.println(makeMessage(message));
         logFile.flush();
     }
 
+    /** 
+     * The done() method should be the last method called in the program.  It does not take any arguments.
+     */
     public static void done() {
 
         String message = String.format("Finished.  Time is %s seconds", getSeconds());
