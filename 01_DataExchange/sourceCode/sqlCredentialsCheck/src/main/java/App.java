1ae40f15ad3bd4f99428d7aaa4eceb0b2332990f
J. Evan Noynaert <noynaert@missouriwestern.edu>
2022-02-14 07:15:40 -0600

    Tweaks, or maybe accidental changes

--- 01_DataExchange/sourceCode/sqlCredentialsCheck/src/main/java/App.java
+++ 01_DataExchange/sourceCode/sqlCredentialsCheck/src/main/java/App.java
@@ -1,9 +1,8 @@
 import com.noynaert.sqlCredentials.SqlCredentials;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.File;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
  * Example for reading sql credentials from an xml file using
@@ -21,13 +20,14 @@ public class App {
         SqlCredentials credentials = new SqlCredentials(args[0]);
         System.out.println(credentials);
         System.out.println("The raw user: " + credentials.getUser());
-        System.out.println("The raw password: " + credentials.getPassword());
         System.out.println("The raw host: " + credentials.getHost());
-        System.out.println("-=-=-=-=-=-=");
         System.out.println("The Password Hint: " + credentials.getPasswordHint());
 
         //Process by Direct Query
         queryString(credentials);
+        System.out.println("-0-0-0-0-0-0-0-0-0-0-0-0");
+        preparedRead("password.txt");
+        System.out.println("-0-0-0-0-0-0-0-0-0-0-0-0");
 
         System.out.println("\nDone!");
     }
@@ -50,6 +50,55 @@ public class App {
             con.close();
         } catch (Exception e) {
             System.err.println("ERROR:\n" + e.getMessage());
+        }
+    }
+
+    public static void preparedRead(String passwordFileName) {
+        final String user = "csc";
+        final String host = "woz.cs.missouriwestern.edu";
+        String password;
+        String connectString = String.format("jdbc:mysql://%s:3306/misc", host);
+        System.out.printf("The connection string is \"%s\"\n", connectString);
+
+        try {
+            Scanner passwordFile = new Scanner(new File(passwordFileName));
+            password = passwordFile.next();
+            System.out.println("The password is " + password);
+
+            // get connection
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    connectString, user, password);
+
+            //Make the query
+//            Scanner kbd = new Scanner(System.in);
+//            System.out.print("What field?");
+//            String field = kbd.next();
+//            System.out.print("What criteria, or % for default");
+//            String criteria = kbd.next();
+//            kbd.close();
+
+            String s1="capital_city";
+            String s2="A%";
+            //Prepare the statement
+            String query = "select  state, capital_city from states where capital_city like ? ORDER BY capital_city";
+            PreparedStatement stmt;
+            stmt = con.prepareStatement(query);
+            stmt.setString(1,s2);
+            `
+
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                String state = rs.getString("state");
+                String fld = rs.getString(s1);
+                System.out.printf("%-15s %s\n",state,fld);
+            }
+            con.close();
+
+            //ResultSet rs = stmt.executeQuery
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
