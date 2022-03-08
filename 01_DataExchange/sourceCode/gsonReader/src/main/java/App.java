dc8af727b1f334c2b8887da249b37ac36fb4c949
J. Evan Noynaert <noynaert@missouriwestern.edu>
2022-03-08 07:10:37 -0600

    start gson reader examples

---
+++ 01_DataExchange/sourceCode/gsonReader/src/main/java/App.java
@@ -0,0 +1,19 @@
+
+/**
+ * Demonstrates reading json from a file and URL.
+ *
+ * For convenience, main() handles all Exceptions.
+ */
+
+public class App {
+    public static void main(String[] args){
+
+        try{
+
+        }catch(Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("\nDone!");
+    }
+}
