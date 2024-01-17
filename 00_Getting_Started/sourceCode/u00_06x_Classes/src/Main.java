f4802cbd3bce6ffa8ea23040c02597ce4e3419f8
J. Evan Noynaert <noynaert@missouriwestern.edu>
2024-01-17 01:09:15 -0600

    code for toString and compareTo

--- 00_Getting_Started/sourceCode/u00_06x_Classes/src/Main.java
+++ 00_Getting_Started/sourceCode/u00_06x_Classes/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person();
         System.out.println(person);
+        Person star = new Person("Fred", 29);
+        System.out.println(star);
         System.out.println("Done!");
     }
 }
--- 00_Getting_Started/sourceCode/u00_06x_Classes/src/edu/missouriwestern/noynaert/people/Person.java
+++ 00_Getting_Started/sourceCode/u00_06x_Classes/src/edu/missouriwestern/noynaert/people/Person.java
@@ -1,9 +1,22 @@
 package edu.missouriwestern.noynaert.people;
 
-public class Person {
+public class Person implements Comparable<Person>{
     private String name;
     private int age;
 
+    //constructors
+    public Person(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+    public Person(String name){
+        this(name, -2);
+    }
+    public Person(){
+        this("Anonymous", -2);
+    }
+
+    //getters and setters
     public String getName() {
         return name;
     }
@@ -17,9 +30,37 @@ public class Person {
     }
 
     public void setAge(int age) {
-        this.age = age;
+       if(age < 0 || age > 120){
+           this.age = Integer.MIN_VALUE;
+       }else{
+           this.age = age;
+       }
     }
     public void setAge(double age){
-        this.age = (int)Math.round(age);
+        setAge((int)Math.round(age));
+    }
+
+    //utility
+    public String toString(){
+        String s = "";
+        if(ageIsValid()) {
+            s = String.format("Name: %s, Age: %d", name, age);
+        }else{
+            s = String.format("Name: %s, Age: --", name);
+        }
+        return s;
+    }
+    public boolean ageIsValid(){
+        return (age >= 0);
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        int result = 0;
+        result = this.name.compareToIgnoreCase(other.name);
+        if(result == 0){
+            result = this.age - other.age;
+        }
+        return result;
     }
 }
