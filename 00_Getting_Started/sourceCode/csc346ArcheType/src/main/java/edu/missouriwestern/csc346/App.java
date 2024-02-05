0369892fddbdba1fb5decf803f3da598ea007bc1
J. Evan Noynaert <noynaert@missouriwestern.edu>
2024-02-05 11:45:32 -0600

    prepared statements in SQL

---
+++ 00_Getting_Started/sourceCode/csc346ArcheType/src/main/java/edu/missouriwestern/csc346/App.java
@@ -0,0 +1,13 @@
+package edu.missouriwestern.csc346;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
