f8142e6cfa3a9dc1eead8edb2096d903e614cd9a
J. Evan Noynaert <noynaert@missouriwestern.edu>
2024-02-07 01:58:49 -0600

    Generics, iterators, and maps

--- 00_Getting_Started/00_070_Generics.md
+++ 00_Getting_Started/00_070_Generics.md
@@ -1,1 +1,34 @@
 # 00.070 Generics
+
+Generics mean subtituting a type.  
+
+Generics are used in things like ArrayLists where a class will work with a specific data type.  
+
+In early versions of Java, we had to do a lot of type casting. Things like ArrayLists worked on the Object class.  We had to do specific typecasts when we needed something specific from the class.  Examples would be 
+
+```java
+public int compareTo(Object o){
+    return (Person)name.compareTo((Person)o.name);
+}
+
+Now we use generic.  It basically eliminates the need to constantly cast types.
+
+```java
+public int compareTo(Person o){
+    return name.compareTo(o.name);
+}
+```
+
+## Standard Letter Types
+
+When Generics first came out, there was a lot of debate about what letters to use.  Now we mostly use things that make sense.  Here is a general guideline, but don't stress out too much on which letter to pick.
+
+Letter|Meaning|Notes
+:---:|:---|:---
+E|Element in a collection or an array|Most commonly used.  It is 
+K|Used in Maps
+V|Used in Maps
+N,X|A numeric type|X isn't standard, but I have seen it used that way.
+S|String| If the item is almost certainly a String, then use it.  But if it is always a String, then why use a generic?
+T|Type|This is a catch-all.  All of the generics are types.  But if the others don't really apply, use T
+U,V,W...|For 2nd, 3rd, 4th data Types
---
+++ 00_Getting_Started/sourceCode/h04_Generics_Iterators/src/main/java/edu/missouriwestern/noynaert/Main.java
@@ -0,0 +1,50 @@
+package edu.missouriwestern.noynaert;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+    private static ArrayList<LocalDate> dateList = new ArrayList<LocalDate>(Arrays.asList(
+            LocalDate.now(),
+            LocalDate.EPOCH,
+            LocalDate.of(2024, 12, 25),
+            LocalDate.of(2000, 01, 01)));
+    private static ArrayList<String> wordList;
+
+    public static void main(String[] args) {
+        wordList = new ArrayList<>(Arrays.asList(args));
+
+        printList(wordList,"List of Words");
+        printList(dateList,"Random Dates");
+
+        wordList.forEach(w -> System.out.println(w));
+
+        Map<String,LocalDate> holidays = new TreeMap<>();
+        holidays.put("Today",LocalDate.now());
+        holidays.put("Time Began",LocalDate.EPOCH);
+        holidays.put("New Year's Day", LocalDate.of(2025, 1, 1));
+        holidays.put("Valentine's Day",LocalDate.of(2024,02,14));
+        holidays.put("Independence Day", LocalDate.of(2024, 7, 4));
+        holidays.put("Christmas Day", LocalDate.of(2024, 12, 25));
+        printMap(holidays);
+
+        var values = new ArrayList<>(holidays.values());
+        Collections.sort(values);
+        printList(values,"Sorted Dates");
+
+
+        System.out.println("Done!");
+    }
+
+    public static <K,V> void printMap(Map<K,V> map){
+        map.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
+    public static<E> void printList(List<E> list, String message){
+        System.out.printf("\n--- %s ---\n",message);
+        for (E element : list) {
+            System.out.println(element);
+        }
+        System.out.printf("--- Printed %d items ---\n",list.size());
+    }
+}
