18cd1f16406885d1960d8b385708e9156d90e1b6
J. Evan Noynaert <noynaert@missouriwestern.edu>
2024-03-20 10:35:19 -0500

    Add code for stopwatch and Runnable

---
+++ 02_AdvancedJava/sourceCode/u02_240_RunnableThreads/src/main/java/com/noynaert/Main.java
@@ -0,0 +1,9 @@
+package com.noynaert;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Done in Main! "+ Thread.currentThread().getName());
+    }
+}
