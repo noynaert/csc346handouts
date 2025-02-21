86182d246295596c76b6fb9d7f5362e9034e94b4
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-02-20 18:44:13 -0600

    Demo slow operation without threading

--- 01_DataExchange/JavaFX/SourceCode2025/javaFX_060_Concurrency/src/main/java/com/noynaert/javafx_060_concurrency/HelloApplication.java
+++ 01_DataExchange/JavaFX/SourceCode2025/javaFX_060_Concurrency/src/main/java/com/noynaert/javafx_060_concurrency/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.noynaert.javafx_060_concurrency;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,31 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+
+        Button button = new Button("Do something that takes a lot of time!");
+        Button poke = new Button("Poke!");
+
+        poke.setOnAction(e -> System.out.println("I got poked!"));
+        button.setOnAction(e -> {
+            try {
+                for (int i = 10; i >= 0; i--) {
+                    System.out.println(i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("Done!");
+
+        });
+
+        root.getChildren().addAll(button, poke);
+
+        Scene scene = new Scene(root, 300 * 1.618, 300);
+        stage.setTitle("Concurrency!");
         stage.setScene(scene);
         stage.show();
     }
