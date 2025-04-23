ce424f18e4798066cd62a7318f975c13fdad994a
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-04-22 23:45:23 -0500

    after video 150 retrieve one record

--- 03_Spring/SourceCode 2025/flagz/src/main/java/edu/missouriwestern/noynaert/flagz/Flagdb.java
+++ 03_Spring/SourceCode 2025/flagz/src/main/java/edu/missouriwestern/noynaert/flagz/Flagdb.java
@@ -1,5 +1,10 @@
 package edu.missouriwestern.noynaert.flagz;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -17,4 +22,13 @@ public class Flagdb {
     public void insert(Flag f){
         db.put(f.getId(), f);
     }
+
+    public Flag findById(int id) {
+        Flag flag = db.get(id);
+        if (flag == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,String.format("Flag %d not found",id));
+        }
+        return flag;
+    }
+
 }
--- 03_Spring/SourceCode 2025/flagz/src/main/java/edu/missouriwestern/noynaert/flagz/FlagzController.java
+++ 03_Spring/SourceCode 2025/flagz/src/main/java/edu/missouriwestern/noynaert/flagz/FlagzController.java
@@ -1,7 +1,11 @@
 package edu.missouriwestern.noynaert.flagz;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import static edu.missouriwestern.noynaert.flagz.FlagzApplication.db;
 
@@ -22,8 +26,17 @@ public class FlagzController {
         String s = "Flags are fun!";
         return s;
     }
-    @GetMapping("/flags")
+    @GetMapping({"/flags/json","/flags"})
     public Iterable<Flag> flags() {
         return db.findAll();
+    }
+    @GetMapping("/flag/{id}")
+    public Flag flag(@PathVariable int id) {
+        Flag flag = db.findById(id);
+        return flag;
+    }
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
